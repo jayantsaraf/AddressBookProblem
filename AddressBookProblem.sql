5c486728c76d33fecfe98439a7266947e1660d82
jayantsaraf <sarafjayant24@gmail.com>
2020-11-02 09:09:50 +0530

    [jayant]Add. created table

--- AddressBookProblem.sql
+++ AddressBookProblem.sql
@@ -2,3 +2,19 @@
 create database Address_Book_Database;
 -- Use Database
 use Address_Book_Database;
+Create Table
+select DB_NAME()
+/*Creating AddressBook table*/
+create table Address_Book
+(
+FirstName varchar(25) not null,
+LastName varchar(25) not null,
+Address varchar(60) not null,
+City varchar(15) not null,
+State varchar(20) not null,
+Zipcode varchar(6) not null,
+PhoneNumber varchar(12) not null,
+Email varchar(20) not null
+);
+/*View table details*/
+select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'Address_Book';
