50453dbfb0815e9d7a0adbcf6b3252ea899f0f78
Jayant Saraf <sarafjayant24@gmail.com>
2020-11-13 12:32:57 +0530

    [jayant]Add. added files to retrieve contact in vs code from database

---
+++ AddressBookProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AddressBookProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Database");
+            AddressRepo repo = new AddressRepo();
+            int loop = 1;
+            while (loop == 1)
+            {
+                Console.WriteLine("Choose \n1. View all records \n2. Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        repo.GetAllContacts();
+                        break;
+                    case 2:
+                        loop = 0;
+                        break;
+                }
+            }
+        }
+    }
+}
